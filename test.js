261d6675b6e4ea6fe8b8037955aaeef61cb7cfbf
Dane Pieri <danepieri@Danes-Powerbook-G4.local>
2016-07-28 18:11:02 -0700

    More unit tests

--- test.js
+++ test.js
@@ -3,9 +3,15 @@ var utils = require('./lib/utils.js').default;
 
 exports.stateNames = function(test) {
   test.expect(2);
-  console.log('Massachusetts', utils.stateCodeToString("25"));
   test.equal('Massachusetts', utils.stateCodeToString("25"));
-  console.log('North Carolina', utils.stateCodeToString("37"));
   test.equal('North Carolina', utils.stateCodeToString("37"));
+  test.done();
+}
+
+exports.badArguments = function(test) {
+  test.expect(3);
+  test.throws(function() {utils.stateCodeToString(25)}, 'State code must be a string');
+  test.throws(function() {utils.stateCodeToString('2')}, 'State code must be two digits');
+  test.throws(function() {utils.stateCodeToString('99')}, 'No matching state for state code 99');
   test.done();
 }
